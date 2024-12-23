1c17cd87dbbb3818721df1cc853c17a980c9a9e5
Androsh7 <Androsh7@gmail.com>
2024-12-22 18:37:18 -0700

    Added Encryption/Decryption Functionality, Logging for Listeners is broken (WIP), Added experimental TCP Transmitter Features

--- CommConsole.ps1
+++ CommConsole.ps1
@@ -32,18 +32,78 @@ SOFTWARE.
 [int] $dst_port = 0
 [string] $dst_proto = ""
 [int] $transmitter = 0
+$convo_file = "conversations.txt"
+$Sock = New-Object System.Net.Sockets.Socket
+
+<# ================================================================= #>
+<# ======================= ENCRYPT / DECRYPT ======================= #>
+<# ================================================================= #>
+
+# Global variables for encryption
+$global:encryption_enabled = $false
+$global:encryption_key = "defaultkey"
+
+# Function to enable encryption
+function Enable_Encryption {
+    if ($global:encryption_key -eq "default") {
+        Write-Host "ERROR: Encryption key not set" -ForegroundColor Red
+        return
+    }
+    $global:encryption_enabled = $true
+    Write-Host "Encryption enabled" -ForegroundColor Green
+}
+
+# Function to disable encryption
+function Disable_Encryption {
+    $global:encryption_enabled = $false
+    Write-Host "Encryption disabled" -ForegroundColor Green
+}
+
+# Function to set encryption key with masked input
+function Set_Encryption_Key {
+    $Key = Read-Host "Enter encryption key" -AsSecureString
+    $global:encryption_key = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Key))
+    Write-Host "Encryption key set" -ForegroundColor Green
+}
+
+# Encryption function
+function Encrypt_XOR_Message {
+    param (
+        [string] $Message
+    )
+    $Encrypted = ""
+    for ($i = 0; $i -lt $Message.Length; $i++) {
+        $Encrypted += [char]($Message[$i] -bxor $global:encryption_key[$i % $global:encryption_key.Length])
+    }
+    return $Encrypted
+}
+
+function Decrypt_XOR_Message {
+    param (
+        [string] $Message
+    )
+    $Decrypted = ""
+    for ($i = 0; $i -lt $Message.Length; $i++) {
+        $Decrypted += [char]($Message[$i] -bxor $global:encryption_key[$i % $global:encryption_key.Length])
+    }
+    return $Decrypted
+}
+
+<# ================================================================= #>
+<# ======================== UDP TRANSMITTER ======================== #>
+<# ================================================================= #>
 
 function Start_UDP_Transmitter {
     try {
         # Credit for Transmitter Socket Code: Thomas Lee - tfl@psp.co.uk - http://www.pshscripts.blogspot.com 
         $global:dst_ip = Read-Host "Destination IP"
         $global:dst_port = [int](Read-Host "Destination Port")
-        $global:dst_proto = "(UDP)"
+        $global:dst_proto = "UDP"
 
         $Address = [system.net.IPAddress]::Parse($dst_ip)
 
         # Create IP Endpoint
-        $End = New-Object System.Net.IPEndPoint $address, $dst_port
+        $End = New-Object System.Net.IPEndPoint $Address, $dst_port
 
         # Create Socket
         $saddrf = [System.Net.Sockets.AddressFamily]::InterNetwork
@@ -70,60 +130,223 @@ function Start_UDP_Transmitter {
     }
 
 }
+
 function Stop_UDP_Transmitter {
     try {
         $global:sock.Close()
-        $global:transmitter = $false
-        $global:dst_ip = ""
-        $global:dst_port = 0
-        $global:dst_proto = ""
-        $global:prompt = ">"
-        Write_Host "Successfully closed the UDP transmitter" -ForegroundColor Blue
+        Write-Host "Successfully stopped the UDP transmitter" -ForegroundColor Blue
     }
     catch {
-        Write-Host "ERROR: could not stop UDP transmitter" -ForegroundColor Red
-        $global:transmitter = $false
-        $global:prompt = ">"
+        Write-Host "ERROR: could not stop the UDP transmitter" -ForegroundColor Red
+    }
+
+    # note: that regardless of whether or not the close was successful, the transmitter is marked as inactive
+    $global:transmitter = $false
+    $global:dst_ip = ""
+    $global:dst_port = 0
+    $global:dst_proto = ""
+    $global:prompt = ">"
+}
+
+function Transmit_UDP_Message {
+    param (
+        $Message
+    )
+    try {
+        # Check if encryption is enabled
+        if ($global:encryption_enabled) {
+            $EncryptedMessage = Encrypt_XOR_Message -Message $Message
+            $DecryptedMessage = $Message
+            $Message = $EncryptedMessage
+        }
+
+        # Create encoded buffer
+        $Enc = [System.Text.Encoding]::ASCII
+        $Buffer = $Enc.GetBytes($Message)
+
+        # Send the buffer via the established socket
+        if ($Sock.Connected) {
+            $Sock.Send($Buffer)
+        } else {
+            Write-Host "ERROR: Socket is not connected" -ForegroundColor Red
+        }
+
+        # Informational message for length
+        $length = $Message.Length
+        $date = Get-Date -UFormat "%m/%d/%Y %R UTC%Z"
+        Write-Host "SENT ${length} Characters AT ${date}" -ForegroundColor Green
+
+        # Log the transmission to the convo_file
+        Add-Content -Path $convo_file -Value "----- SENT TO ${dst_ip}:${dst_port} ${dst_proto} AT ${date} -----"
+        Add-Content -Path $convo_file -Value "$Message"
+        if ($global:encryption_enabled) {
+            Add-Content -Path $convo_file -Value "----- Decrypted Message Below -----"
+            Add-Content -Path $convo_file -Value "$DecryptedMessage"
+        }
+    }
+    catch {
+        Write-Host "ERROR: could not send message" -ForegroundColor Red
     }
 }
 
-# Receiver
-$convo_file = "conversations.txt"
+function Test_Connection {
+    if ($transmitter) {
+        try {
+            # Test UDP connection
+            $global:sock.Send([System.Text.Encoding]::ASCII.GetBytes("Test"))
+            Write-Host "UDP connection to ${dst_ip}:${dst_port} ${dst_proto} is successful" -ForegroundColor Green
+        }
+        catch {
+            Write-Host "ERROR: UDP connection to ${dst_ip}:${dst_port} ${dst_proto} is unsuccessful" -ForegroundColor Red
+        }
+
+        try {
+            # Test ICMP ping
+            $ping = New-Object System.Net.NetworkInformation.Ping
+            $reply = $ping.Send($dst_ip)
+            if ($reply.Status -eq [System.Net.NetworkInformation.IPStatus]::Success) {
+                Write-Host "Ping to ${dst_ip} is successful" -ForegroundColor Green
+            }
+            else {
+                Write-Host "ERROR: Ping to ${dst_ip} is unsuccessful" -ForegroundColor Red
+            }
+        }
+        catch {
+            Write-Host "ERROR: Unable to ping ${dst_ip}" -ForegroundColor Red
+        }
+    } else {
+        Write-Host "ERROR: Transmitter is not active" -ForegroundColor Red
+    }
+}
+
+<# ================================================================= #>
+<# ======================== TCP TRANSMITTER ======================== #>
+<# ================================================================= #>
+
+function Start_TCP_Transmitter {
+    try {
+        # Get destination IP and port from user
+        $global:dst_ip = Read-Host -Prompt "Destination IP"
+        $global:dst_port = [int](Read-Host -Prompt "Destination Port")
+        $global:dst_proto = "(TCP)"
+
+        $Address = [system.net.IPAddress]::Parse($dst_ip)
+
+        # Create IP Endpoint
+        $End = New-Object System.Net.IPEndPoint $Address, $dst_port
+
+        # Create Socket
+        $saddrf = [System.Net.Sockets.AddressFamily]::InterNetwork
+        $Stype = [System.Net.Sockets.SocketType]::Stream
+        $Ptype = [System.Net.Sockets.ProtocolType]::TCP
+        $global:Sock = New-Object System.Net.Sockets.Socket $saddrf, $stype, $ptype
+
+        # Connect to socket
+        $global:sock.Connect($End)
+
+        # Mark transmitter as working
+        $global:transmitter = 1
+        
+        Write-Host "Successfully started the TCP transmitter" -ForegroundColor Blue
+        $global:prompt = "${dst_ip}:${dst_port} ${dst_proto}>"
+    }
+    catch {
+        Write-Host "ERROR: could not setup TCP transmitter" -ForegroundColor Red
+        $global:transmitter = $false
+    }
+}
+
+function Stop_TCP_Transmitter {
+    try {
+        $global:sock.Close()
+        Write-Host "Successfully stopped the TCP transmitter" -ForegroundColor Blue
+    }
+    catch {
+        Write-Host "ERROR: could not stop the TCP transmitter" -ForegroundColor Red
+    }
+
+    # note: that regardless of whether or not the close was successful, the transmitter is marked as inactive
+    $global:transmitter = $false
+    $global:dst_ip = ""
+    $global:dst_port = 0
+    $global:dst_proto = ""
+    $global:prompt = ">"
+}
+
+function Send_TCP_Message {
+    param (
+        $Message
+    )
+    try {
+        # Create encoded buffer
+        $Enc = [System.Text.Encoding]::ASCII
+        $Buffer = $Enc.GetBytes($Message)
+
+        # Send the buffer via the established socket
+        if ($Sock.Connected) {
+            $Sock.Send($Buffer)
+        } else {
+            Write-Host "ERROR: Socket is not connected" -ForegroundColor Red
+        }
+
+        # Informational message for length
+        $length = $Message.Length
+        $date = Get-Date -UFormat "%m/%d/%Y %R UTC%Z"
+        Write-Host "SENT ${length} Characters AT ${date}" -ForegroundColor Green
+
+        # Log the transmission to the convo_file
+        Add-Content -Path $convo_file -Value "----- SENT TO ${dst_ip}:${dst_port} ${dst_proto} AT ${date} -----"
+        Add-Content -Path $convo_file -Value "$Message"
+    }
+    catch {
+        Write-Host "ERROR: could not send message" -ForegroundColor Red
+    }
+}
+
+<# ================================================================= #>
+<# ========================= UDP RECEIVER ========================== #>
+<# ================================================================= #>
 
 # This is the code that opens in another window
 $UDP_Receiver = {
-    $port = Read-Host "Select the UDP Listening Port"
-    $port = [int]$port
-    $udpClient = New-Object System.Net.Sockets.UdpClient($port) # Create a UDP client
-    $udpClient.Client.ReceiveTimeout = 1000 # Set a timeout for receiving data (optional)
-    $remoteEndPoint = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Any, 0) # Create a remote endpoint to hold the sender's information
+    $port = [int](Read-Host "Select the UDP Listening Port")
+    try {
+        $udpClient = New-Object System.Net.Sockets.UdpClient($port)
+        $udpClient.Client.ReceiveTimeout = 1000
+    } catch {
+        Write-Host "ERROR: Unable to establish a listening port on ${port}" -ForegroundColor Red
+        Exit
+    }
+
+    $remoteEndPoint = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Any, 0)
 
     $date = Get-Date
     Clear-Host
     Write-Host "---------------------- UDP_LISTENER on port ${port} ----------------------" -ForegroundColor Blue
     Write-Host "Writing conversation to $convo_file" -ForegroundColor Blue
     Write-Host "Started Recording at ${date}" -ForegroundColor Blue
-    powershell.exe Write-Host "----- Started Recording at ${date} on UDP port ${port} -----" >> $convo_file
+    # Add-Content -Path $convo_file -Value "----- Started Recording -----"
 
-    try{
+    try {
         while ($true) {
             try {
-                $receiveBytes = $udpClient.Receive([ref]$remoteEndPoint) # Receive a datagram 
-                $receivedData = [Text.Encoding]::ASCII.GetString($receiveBytes) # Convert the received byte array to a string
+                $receiveBytes = $udpClient.Receive([ref]$remoteEndPoint)
+                $receivedData = [Text.Encoding]::ASCII.GetString($receiveBytes)
+                $senderIP = $remoteEndPoint.Address.ToString()
+                $senderPort = $remoteEndPoint.Port
 
                 $date = Get-Date
-                Write-Host $convo_file -Append -InputObject "----- RECEIVED AT ${date} -----"
-                powershell.exe Write-Host $convo_file -Append -InputObject "----- RECEIVED AT ${date} -----" >> $convo_file
-                Write-Host $convo_file -Append -InputObject ${receivedData}
-                powershell.exe Write-Host $convo_file -Append -InputObject ${receivedData} >> $convo_file
+                Write-Host "----- RECEIVED FROM ${senderIP}:${senderPort} AT ${date} -----" -ForegroundColor Green
+                Write-Host "$receivedData"
+
+                # Add-Content -Path $convo_file -Value "----- RECEIVED FROM ${senderIP} AT ${date} -----"
+                # Add-Content -Path $convo_file -Value "$receivedData"
             } catch {}
         }
     } finally {
         Write-Host "---------------------- Stopped Recording on port ${port} ----------------------" -ForegroundColor Blue
-        powershell.exe Write-Host "----- Stopped Recording at ${date} on UDP port ${port} -----" >> $convo_file
-        #close the UDP listener when exiting the loop
+        # Add-Content -Path $convo_file -Value "----- Stopped Recording at ${date} on UDP port ${port} -----"
         $udpClient.Close()
-        Start-Sleep 2
     }
 }
 
@@ -136,45 +359,74 @@ function UserCommands {
         # Command Menu
         "#help" { 
             Write-Host "#help - opens the help menu
-#start - begins the udp transmitter
-#stop / #end - stops the udp transmitter
-#listen- opens a udp listener window
+#UDP_start - begins the udp transmitter
+#UDP_stop / #UDP_end - stops the udp transmitter
+#UDP_listen- opens a udp listener window
+#TCP_start - begins the tcp transmitter
+#TCP_stop / #TCP_end - stops the tcp transmitter
 #info - gives info on the transmitter
-#test - tests the current transmitter
+#UDP_test - tests the current UDP transmitter
 #set_file - designates a file to store the conversation
 #clear_file - clears the conversation file
 #read_file - opens a terminal that reads the conversation
+#enable_encryption - enables message encryption
+#disable_encryption - disables message encryption
+#set_key - sets the encryption key
 #q / #quit - exits the program and closes the socket" -ForegroundColor Green
         }
         # setup the udp transmitter
-        "#start" { Start_UDP_Transmitter }
+        "#UDP_start" { Start_UDP_Transmitter }
         # close the udp transmitter
-        "#end" { Stop_UDP_Transmitter }
-        "#stop" { Stop_UDP_Transmitter }
+        "#UDP_end" { Stop_UDP_Transmitter }
+        "#UDP_stop" { Stop_UDP_Transmitter }
         # opens a udp listener window
-        "#listen" {
-            $pwd = [string](Get-Location)
-            Start-Process -FilePath powershell.exe -WorkingDirectory $pwd -ArgumentList "Set-Variable -Name `"convo_file`" -Value `"conversations.txt`"", "$UDP_Receiver"
+        "#UDP_listen" {
+            Start-Process -FilePath powershell.exe -WorkingDirectory $pwd -ArgumentList "Set-Variable -Name `"convo_file`" -Value `"conversations.txt`"; $UDP_Receiver"
         }
+        # setup the tcp transmitter
+        "#TCP_start" { Start_TCP_Transmitter }
+        # close the tcp transmitter
+        "#TCP_end" { Stop_TCP_Transmitter }
+        "#TCP_stop" { Stop_TCP_Transmitter }
         # prints the transmission info
         "#info" {
-            Write-Host "Dest IP: ${dst_ip}" -ForegroundColor Green
-            Write-Host "Dest Port: ${dst_port}" -ForegroundColor Green
-            Write-Host "Protocol: ${proto}" -ForegroundColor Green
+            if ($transmitter) {
+                Write-Host "Dest IP: ${global:dst_ip}" -ForegroundColor Green
+                Write-Host "Dest Port: ${global:dst_port}" -ForegroundColor Green
+                Write-Host "Protocol: ${global:dst_proto}" -ForegroundColor Green
+            }
+            else {
+                Write-Host "Transmitter is not active" -ForegroundColor Green
+            }
+            Write-Host "Conversation File: ${convo_file}" -ForegroundColor Green
         }
         # tests the connection to the target
-        "#test" {
+        "#UDP_test" {
+            Test_Connection
         }
         # setup the save file
         "#set_file" {
+            $global:convo_file = Read-Host "Enter the file name"
         }
         # clear the conversation file
         "#clear_file" {
-            Out-File $convo_file
+            Out-File -FilePath $convo_file -InputObject $null
         }
         # open file reader
         "#read_file" {
 
+        }
+        # enable encryption
+        "#enable_encryption" {
+            Enable_Encryption
+        }
+        # disable encryption
+        "#disable_encryption" {
+            Disable_Encryption
+        }
+        # set encryption key
+        "#set_key" {
+            Set_Encryption_Key
         }
         # quit program
         "#q" { Exit }
@@ -192,7 +444,8 @@ Write-Host "---------------------- By Androsh7 ----------------------" -Foregrou
 
 try {
     while ($true) {
-        $UserInput = Read-Host $global:prompt
+        Write-Host $global:prompt -NoNewline
+        $UserInput = Read-Host
 
         # Runs commands starting with "#"
         if ($UserInput -imatch "^#") { 
@@ -200,22 +453,11 @@ try {
         }
 
         # If the transmitter is setup, attempt to transmit the message
-        elseif ($transmitter) {
-            # Create encoded buffer
-            $Enc = [System.Text.Encoding]::ASCII
-            $Buffer = $Enc.GetBytes($UserInput)
-
-            # Send the buffer via the established socket
-            $Sent = $global:Sock.Send($Buffer)
-
-            # Informational message for length
-            $length = $UserInput.Length
-            $date = Get-Date -UFormat "%m/%d/%Y %R UTC%Z"
-            Write-Host "SENT ${length} Characters AT ${date}" -ForegroundColor Green
-            
-            # writes to the conversation file
-            Out-File $convo_file -Append -InputObject "----- SENT TO ${dst_ip}:${dst_port} ${dst_proto} AT ${date} -----"
-            Out-File $convo_file -Append -InputObject $UserInput
+        elseif ($global:transmitter -and $global:dst_proto -eq "UDP") {
+            Transmit_UDP_Message $UserInput
+        }
+        elseif ($global:transmitter -and $global:dst_proto -eq "TCP") {
+            Send_TCP_Message $UserInput
         }
 
         # If the transmitter is not setup, give an error
